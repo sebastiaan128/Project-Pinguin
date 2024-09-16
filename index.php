a2292d7920a059af0dbf12d5955c22114c85b989
FlorisEllenbroek <ellenbroekfloris605@gmail.com>
2024-09-16 14:18:17 +0200

    ...

--- index.php
+++ index.php
@@ -29,17 +29,12 @@
             </div>
             <div class="col-md-12 col-lg-6 me-auto order-1 order-lg-2">
                 <p class="tekst mx-auto w-100 w-lg-75">
-                    Pinguïns of vetganzen zijn een orde van niet-vliegende zeevogels die alleen voorkomen op het zuidelijk
-                    halfrond. De pinguïns behoren tot de orde Sphenisciformes, klasse Aves (vogels). Alle moderne pinguïns
-                    behoren tot de familie van de Spheniscidae, maar er zijn uitgestorven soorten die buiten deze kroongroep
-                    vallen.<br><br>
-                    De pinguïns zijn gemakkelijk te onderscheiden van andere vogels en zijn volledig aangepast aan extreme
-                    koude en het leven in de zee. Ze hebben bijvoorbeeld een warm verenkleed. Pinguïns gebruiken hun
-                    vleugels om door het water te vliegen, net zoals andere vogels door de lucht vliegen. Ze kunnen met een
-                    snelheid tot 30 km per uur door het water vliegen. Volgens sommige woordenboeken, zoals de Oxford
-                    English Dictionary, komt de naam "pinguïn" waarschijnlijk van het Welsh pen gwyn. De naam werd
-                    oorspronkelijk gebruikt voor de reuzenalk, de inmiddels uitgestorven tegenhanger (geen nauwe verwant)
-                    van de pinguïn op het noordelijk halfrond.
+                De pinguïn is een van de meest herkenbare en geliefde dieren ter wereld. 
+                Deze niet-vliegende vogel leeft voornamelijk op het zuidelijk halfrond, waarbij de meeste soorten voorkomen in koude, ijzige omgevingen zoals Antarctica. 
+                Ondanks hun beperkte vliegvermogen, zijn pinguïns uitstekende zwemmers. 
+                Ze hebben zich aangepast aan het leven in zee, waar ze zich behendig door het water bewegen om voedsel, zoals vis en krill, te vangen. 
+                Hun zwarte rug en witte buik zorgen voor camouflage in het water tegen roofdieren. 
+                Pinguïns staan ook bekend om hun sociale gedrag en het vormen van grote kolonies, waar ze gezamenlijk broeden en elkaar beschermen tegen vijanden en extreme weersomstandigheden.
                 </p>
             </div>
     </div>
