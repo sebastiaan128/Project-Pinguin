68bf9539425220014c0e527164f4e29cc12d314d
Sebastiaan Balfoort <581150@edu.rocmn.nl>
2024-09-16 09:59:29 +0200

    chore: Update index.php with new content and remove unnecessary code

---
+++ navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/Te_veel_vrije_tijd/Project-Pinguin">Pinguin</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link text-white" href="/Te_veel_vrije_tijd/Project-Pinguin/Habitat-Leefomgeving.php">Habitat - Leefomgeving</a>
+                <a class="nav-link text-white" href="/Te_veel_vrije_tijd/Project-Pinguin/Voeding-dieet.php">Voeding - Dieet</a>
+                <a class="nav-link text-white" href="/Te_veel_vrije_tijd/Project-Pinguin/Gedrag-sociaal">Gedrag - Sociale structuur</a>
+            </div>
+        </div>
+    </div>
+</nav> 
