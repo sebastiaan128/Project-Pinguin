f681cddf491022966693ff2698c7aa63ad754f93
Sebastiaan Balfoort <581150@edu.rocmn.nl>
2024-09-16 08:58:34 +0200

    Remove unnecessary index.html file

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>    
+</head>
+<body class="bg-black">
+<div class="container text-center">
+  <div class="row align-items-center">
+    <div class="col text-white">
+    <p></p>
+    </div>
+    <div class="col text-white mt-5">
+      <h1>Pinguin</h1>
+      <img class="border-radius w-100px h-[100px] object-cover" src="IMG/Pinguin.png" alt="">
+        </div>
+  </div>
+</div>
+</body>
+</html>
